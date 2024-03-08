098f2289a577baf11f1afd484046bcc63f3322d9
KevinDorado7 <157407295+KevinDorado7@users.noreply.github.com>
2024-03-07 20:58:44 -0600

    Updated main.py
    Fixed de csvpath

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-csvpath=os.path.join("/Users/kevindorado/Desktop/Python_challenge/PyPoll/Resources/election_data.csv")
+csvpath=os.path.join("/Python_challenge/PyPoll/Resources/election_data.csv")
 
 total_votes = 0
 
